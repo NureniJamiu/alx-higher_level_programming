5c7a00af99c5c1ec0fd1129fa10d3a45a54904ab
Dhulnurein <nurenijamiu951@gmail.com>
2022-10-11 22:35:19 +0100

    python exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    number = 0
-    for i in range(x):
+    index = 0
+    while True:
         try:
-            print(my_list[i], end="")
-            number += 1
+            if index < x:
+                print(my_list[index], end='')
+                index += 1
+            else:
+                print()
+                return index
         except IndexError:
-            break
-    print()
-    return number
+            print()
+            return index
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_function(fct, *args):
     try:
-        result = fct(*args)
-        return result
-    except Exception as error:
-        import sys
+        return fct(*args)
+    except (ZeroDivisionError, ValueError, TypeError, IndexError) as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return None
